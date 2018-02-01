6ae98b8c660f3296d35e8475fb9b1b2764b41862
eychcue <hq.haroon1@gmail.com>
2018-02-01 00:15:12 -0500

    added threading

--- cryptopiaBot2.py
+++ cryptopiaBot2.py
@@ -3,7 +3,8 @@ import time
 import datetime
 import json
 import sqlite3
-
+from threading import Thread
+from time import sleep
 
 r = requests.get("https://www.cryptopia.co.nz/api/GetMarkets/BTC").json()
 
@@ -13,9 +14,6 @@ for i in range(len(r["Data"])):
     pairID = r["Data"][i]["TradePairId"]
     listOFpairIDs.append(pairID)
     curr_dict.update({str(pairID): i})
-
-
-
 
 
 
@@ -69,54 +67,18 @@ def getSellBaseVolume(TradePairId):
 
 
 
-
-
-
-
-
-
-							#*** if you wanted it printed uncomment these lines ***#
-# for i in range(len(listOFpairIDs)):
-# 	pairID = r["Data"][i]["TradePairId"]
-# 	print pairID
-# 	print(getLabel(pairID))
-# 	print(getAskPrice(pairID))
-# 	print(getBidPrice(pairID))
-# 	print(getLow(pairID))
-# 	print(getHigh(pairID))
-# 	print(getVolume(pairID))
-# 	print(getLastPrice(pairID))
-# 	print(getLastPrice(pairID))
-# 	print(getBuyVolume(pairID))
-# 	print(getBuyVolume(pairID))
-# 	print(getSellVolume(pairID))
-# 	print(getChange(pairID))
-# 	print(getOpen(pairID))
-# 	print(getClose(pairID))
-# 	print(getBaseVolume(pairID))
-# 	print(getBuyBaseVolume(pairID))
-# 	print(getSellBaseVolume(pairID))
-# 	print("\n")
-
-
-
-
-
 conn = sqlite3.connect("Coins.db")
 c = conn.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS Cryptopia(tradePairID REAL, label TEXT, askPrice REAL, bidPrice REAL, low REAL, High REAL, volume REAL, lastPrice REAL, buyVolume REAL, sellVolume REAL, change REAL, open REAL, close REAL, baseVolume REAL, buyBaseVolume REAL, sellBaseVolume REAL)") 	#ADD THESE: occuranceID REAL, time, buyBaseVolume REAL, name TEXT
 
 
-# or if you wanted just the calls for functions, this will loop through all the coins and there shouldnt be any future errors
-TimeStamp = 0
+def threaded_function():
 
-for x in range(1000):
+    for x in range(1000):
 	r = requests.get("https://www.cryptopia.co.nz/api/GetMarkets/BTC").json()
 
 	conn = sqlite3.connect("Coins.db")
 	c = conn.cursor()
-
-
 
 	for i in range(len(listOFpairIDs)):
 		pairID = r["Data"][i]["TradePairId"]
@@ -137,14 +99,18 @@ for x in range(1000):
 		SellBaseVolume = getSellBaseVolume(pairID)
 																#***Enter all data of every coin in Cryptopia onto a sqlite database***#
 		c.execute("INSERT INTO Cryptopia VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);", (pairID,Label,AskPrice,BidPrice,Low,High,Volume,LastPrice,BuyVolume,SellVolume,Change,Open,Close,BaseVolume,BuyBaseVolume,SellBaseVolume))
-	TimeStamp += 1
 	print "Got it", x
 
 	conn.commit()
 	# c.close()
 	# conn.close()
+        sleep(1)
 
-	time.sleep(2)
 
+if __name__ == "__main__":
+    thread = Thread(target = threaded_function)
+    thread.start()
+    thread.join()
+    print ("Threading has finished") #not going to happen lol
 
 
